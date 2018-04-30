7fffcc70aa7cc174b89966b6f3a4908df40f08c2
Netman <4051889+augustsix@users.noreply.github.com>
2018-04-30 12:29:48 -0700

    created snmp community audit

--- 2.Easy_Wins/ansible/filter_plugins/myfilters.py
+++ 2.Easy_Wins/ansible/filter_plugins/myfilters.py
@@ -6,10 +6,17 @@ class FilterModule(object):
 
     def filters(self):
         return {
-            'mdtohtml':self.mdtohtml
+            'mdtohtml':self.mdtohtml,
+            'find_communities':self.find_communities
         }
 
     def mdtohtml(self, mdtext):
         import mistune
         markdown = mistune.Markdown()
         return markdown(mdtext)
+
+    def find_communities(self,cmd_output):
+        communities = []
+        for line in cmd_output:
+            communities.add(line.split()[2])
+        return communities
